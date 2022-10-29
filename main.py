1b377115924d4a55648affb1473636e7e940c03f
Aaron Griffin <Aaron@Aarons-Macbook-Air.local>
2022-10-29 21:37:56 +0100

    homepage window added to

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import PySimpleGUI as gui
 
 def main():
-    gui.theme('DarkAmber')
+    home_title = 'Home'
+    
+    gui.theme('DarkBlue12')
 
-    layout = [ [gui.Text('Expense Tracker')],
+    home_layout = [ [gui.Titlebar(home_title)],
+                [gui.Text('Expense Tracker')],
                 [gui.Text('Add to Today\'s spending'), gui.Text('Add to this Week\'s spending')],
                 [gui.InputText(), gui.InputText()],
                 [] ]
     
-    window = gui.Window('Home', layout)
+    home_window = gui.Window(home_title, home_layout)
 
     while True:
-        event, values = window.read()
+        event, values = home_window.read()
         if event == gui.WINDOW_CLOSED:
             break
     
-    window.close()
+    home_window.close()
 
 if __name__ == '__main__':
     main()
