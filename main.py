cbb66e1b18c76a991e047877acefcf96f949a441
Aaron Griffin <Aaron@Aarons-Macbook-Air.local>
2022-11-04 20:24:07 +0000

    basic home window gui template complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as gui
 
+def gtp(left, right, top, bottom):
+    return ((left, right), (top, bottom))
+
 def main():
     home_title = 'Home'
     
@@ -7,13 +10,18 @@ def main():
    
 
     home_layout = [ [gui.Titlebar(home_title)],
-                [gui.Text('Expense Tracker')],
+                [gui.Text('Expense Tracker', pad=gtp(240,240,15,15), font='TimesNewRoman 21')],
                 [gui.Text('Add to Today\'s spending',  pad=((75,0), (10,10))), gui.Text('Add to this Week\'s spending', pad=((100,0), (10,10)))],
                 [gui.InputText(), gui.InputText()],
                 [gui.Text('Today\'s spending'), gui.Text('[Month]\'s spending')],
                 [gui.Text('£[value1]'), gui.Text('£[value2]')],
-                [gui.Graph((200,200), (0,0), (200,200)), gui.Graph((200,200), (0,0), (200,200))], 
-                [] ]
+                [gui.Graph((200,200), (0,0), (200,200), background_color='red'), gui.Graph((200,200), (0,0), (200,200), background_color='green')], 
+                [gui.Text('[Year\'s] spending')],
+                [gui.Text('£[valueY]')], 
+                [gui.Graph((200,200), (0,0), (200,200), background_color='yellow')],
+                [gui.Text('See a more detailed breakdown of your spending. Edit your spending.'), gui.Button('Go to Spending')], 
+                [gui.Text('Get insights into your spending habits.'),  gui.Button('Go to Insights')], 
+                [gui.Text('Customise your preferences and settings for this application.'),  gui.Button('Go to Settings')] ]
     
     home_window = gui.Window(home_title, home_layout)
 
