49f8f4b6059f1a1abef94a6b09f47a243f830fd2
Aaron Griffin <Aaron@Aarons-Macbook-Air.local>
2022-11-04 18:08:54 +0000

    progress made

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def main():
     home_title = 'Home'
     
     gui.theme('DarkBlue12')
+   
 
     home_layout = [ [gui.Titlebar(home_title)],
                 [gui.Text('Expense Tracker')],
-                [gui.Text('Add to Today\'s spending'), gui.Text('Add to this Week\'s spending')],
+                [gui.Text('Add to Today\'s spending',  pad=((75,0), (10,10))), gui.Text('Add to this Week\'s spending', pad=((100,0), (10,10)))],
                 [gui.InputText(), gui.InputText()],
+                [gui.Text('Today\'s spending'), gui.Text('[Month]\'s spending')],
+                [gui.Text('£[value1]'), gui.Text('£[value2]')],
+                [gui.Graph((200,200), (0,0), (200,200)), gui.Graph((200,200), (0,0), (200,200))], 
                 [] ]
     
     home_window = gui.Window(home_title, home_layout)
